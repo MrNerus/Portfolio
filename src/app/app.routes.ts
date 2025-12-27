e83d78baa066d4bd6ac66ad5080b8265af30cd26
surendra-ims <surendra@imssoftware.com.np>
2025-12-27 10:10:42 +0545

    Github Page

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-
-import { Routes } from '@angular/router';
-import { Hero } from './page/hero/hero';
-
-export const routes: Routes = [
-    { path: '', component: Hero },
-];
--- src/app/components/path.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface PathInterface {
-  startX: number;
-  startY: number;
-  endX: number;
-  endY: number;
-  direction: number;
-}
